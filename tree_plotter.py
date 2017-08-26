377c222e7255d513b334058ac91c6102c427e5a7
noeticwxb <noetic_wxb@126.com>
2017-08-26 23:15:05 +0800

    add plot tree
    add plot tree

--- tree_plotter.py
+++ tree_plotter.py
@@ -10,12 +10,21 @@ def plot_node(node_text,center_pt,parent_pt,node_type):
     create_plot.ax1.annotate(node_text, xy=parent_pt ,xycoords='axes fraction', xytext=center_pt, textcoords='axes fraction', va='center', ha='center', bbox=node_type, arrowprops=Arrow_Args)
 
 
-def create_plot():
+def plot_mid_text(tex,center_pt,parent_pt):
+    mid_x = (parent_pt[0]-center_pt[0]) / 2.0 + center_pt[0]
+    mid_y = (parent_pt[1]-center_pt[1]) / 2.0 + center_pt[1]
+    create_plot.ax1.text(mid_x,mid_y,tex)
+
+
+def create_plot(my_tree):
     fig = plt.figure(1,facecolor='white')
     fig.clf()
     create_plot.ax1 = plt.subplot(111,frameon=False)
-    plot_node('Decision Node', (0.5,0.1), (0.1,0.5), DecisionNode)
-    plot_node('Leaf Node', (0.8,0.1), (0.3,0.8), LeafNode)
+    #plot_node('Decision Node', (0.5,0.1), (0.1,0.5), DecisionNode)
+    #plot_node('Leaf Node', (0.8,0.1), (0.3,0.8), LeafNode)
+    X_OFF = 1.0 / (get_num_leafs(my_tree)-1 )
+    Y_OFF = 1.0 / get_num_depth(my_tree)
+    plot_tree(my_tree, 0, 1, 1, (0.5, 1), X_OFF, Y_OFF)
     plt.show()
 
 
@@ -78,7 +87,31 @@ def retrieve_tree(i):
     return list_trees[i]
 
 
+def plot_tree(my_tree, x_pos_begin, x_pos_end, y_pos, parent_pt,X_OFF,Y_OFF):#type:(dict,float,float,float,tuple,float,float)->void
+    node_str = my_tree.keys()[0]
+    node_pos = ((x_pos_begin+x_pos_end)/2.0,y_pos)
+    plot_node(node_str,node_pos,parent_pt,DecisionNode)
+    child_dict = my_tree[node_str] #type:dict
+    x_child_pos = x_pos_begin
+    y_child_pos = y_pos - Y_OFF
+    for key,child_node in child_dict.iteritems():
+        if( type(child_node).__name__=='dict'):
+            x_child_range = X_OFF * (get_num_leafs(child_node)-1)
+            x_child_end = x_child_pos + x_child_range
+            plot_tree(child_node,
+                      x_child_pos,
+                      x_child_end,
+                      y_child_pos,
+                      node_pos,
+                      X_OFF,Y_OFF)
+            plot_mid_text(key, ((x_child_pos+x_child_end)/2.0,y_child_pos ),node_pos)
+            x_child_pos = x_child_pos + x_child_range + X_OFF
+        else:
+            plot_node(child_node, (x_child_pos,y_child_pos), node_pos, LeafNode)
+            plot_mid_text(key, (x_child_pos,y_child_pos), node_pos)
+            x_child_pos = x_child_pos +  X_OFF
+
 if __name__ == '__main__':
-    #create_plot()
-    print get_num_leafs(retrieve_tree(1))
-    print get_num_depth(retrieve_tree(1))
+    create_plot(retrieve_tree(1))
+    #print get_num_leafs(retrieve_tree(1))
+    #print get_num_depth(retrieve_tree(1))
