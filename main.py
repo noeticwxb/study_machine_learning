e39414c9d0d26a960f7ccd866a1545626b93b842
noeticwxb <noetic_wxb@126.com>
2018-02-10 17:35:43 +0800

    SVM ok

--- main.py
+++ main.py
@@ -11,13 +11,5 @@ print(aInvertMat)
 print(I_Mat)
 '''
 
-#v = np.mat([[1,2,3],[4,5,6]])
-
-#s = np.mat([2,3]).transpose()
-
-#print(np.multiply(s,v))
-
-y = 1
-x= 1.2
-print(x-y)
+v = np.mat([[0,2,3],[4,0,6],[7,0,9]])
 
