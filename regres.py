b9eb93d74f0cbdb83bfae41b72a50a1a79e8bb9a
noeticwxb <noetic_wxb@126.com>
2018-03-22 11:04:37 +0800

    local wighted linear regres

--- regres.py
+++ regres.py
@@ -24,6 +24,20 @@ class Regres:
         ws = xTx.I * (features.T * lables)
         return  ws
 
+    def local_weight_Linear_regres(self,test_point,features:np.matrix,lables:np.matrix,k=1.0):
+        sample_count,feature_count = np.shape(features)
+        local_weight = np.asmatrix(np.eye(sample_count))
+        k_square = k ** 2
+        for j in range(sample_count):
+            diff_mat = test_point - features[j,:]
+            local_weight[j,j] = np.exp((diff_mat*diff_mat.T)/(-2.0*k_square))
+        xTx = features.T * (local_weight * features)
+        if np.linalg.det(xTx) == 0.0:
+            print("the matrix is singular,can not be inverse")
+            return
+        ws = xTx.I * (features.T *(local_weight* lables))
+        return test_point*ws
+
 
 def test_stand_regres():
     regres = Regres()
@@ -41,13 +55,34 @@ def test_stand_regres():
     plt.show()
 
 
+def test_lwl_regres(k=1.0):
+    regres = Regres()
+    features, lables = regres.load_dataset("data/ch08/ex0.txt")
+    sample_count,feature_count = np.shape(features)
+    labels_predit = np.zeros(sample_count)
+    for i in range(sample_count):
+        labels_predit[i] = regres.local_weight_Linear_regres(features[i],features,lables,k)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(features[:, 1].flatten().getA()[0], lables[:, 0].flatten().getA()[0])
+
+    sordInd = features[:,1].argsort(0)
+    features_sorted = features[sordInd][:,0,:]
+    #print(np.shape(features_sorted))
+    #print(features_sorted)
+    ax.plot(features_sorted[:, 1].flatten().getA()[0], labels_predit[sordInd])
+
+    fig.show()
+
 
 if __name__ == '__main__':
-    test_stand_regres()
+    #test_stand_regres()
     #m = np.asmatrix([[4,3],[2,10],[5,6]])
     #print(np.shape(m))
     #m.sort(1)
     #print(m)
+    test_lwl_regres(0.002)
 
 
 
